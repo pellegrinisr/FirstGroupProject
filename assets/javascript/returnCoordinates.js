7edf04fd9a4b2bd90ad478e4b243d28d4c903d11
Stephen Pellegrini <spellegrini156@gmail.com>
2018-07-10 22:56:37 -0700

    probably gonna delete this branch

--- assets/javascript/returnCoordinates.js
+++ assets/javascript/returnCoordinates.js
@@ -25,19 +25,45 @@ $(document).ready(function() {
                 zoom: 15
                 
             });
-            var searchString = 'restaurants';
-            var placesURL = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=" + searchString + 
-                "&inputtype=textquery&fields=photos,formatted_address,name,opening_hours,rating&locationbias=circle:2000@" + 
-                latCord + ',' + longCord + apiKey;
-            $.ajax({
-                url: placesURL,
-                method: 'GET'
-            }).then(function(response) {
-                console.log(response);
-            });
+            // searchPlacesAPI(latCord, longCord);
+            var request = {
+                location: latCord + ',' + longCord,
+                radius: '500',
+                types: ['restaurant']
+            };
+            var service = new google.maps.places.PlacesService();
+            console.log(service);
+            // service.nearbySearch(request, function() {
+            //     console.log('success');
+            // });
         });
 
+        // var searchString = 'restaurants';
+        // var placesURL = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=" + searchString + 
+        //     "&inputtype=textquery&fields=photos,formatted_address,name,opening_hours,rating&locationbias=circle:2000@" + 
+        //     '34.0736204,-118.4003563' + apiKey;
+        // $.ajax({
+        //     url: placesURL,
+        //     method: 'GET',
+        //     dataType: 'jsonp',
+        //     cache: false,
+        //     success: function(response) {
+        //         console.log(response);
+        //     }
+        // });
     });
+    function searchPlacesAPI(lat, long) {
+        var searchString = 'restaurants';
+        var placesURL = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=" + searchString + 
+            "&inputtype=textquery&fields=photos,formatted_address,name,opening_hours,rating&locationbias=circle:2000@" + 
+            lat + ',' + long + apiKey;
+        $.ajax({
+            url: placesURL,
+            method: 'GET'
+        }).then(function(response) {
+            console.log(response);
+        });
+    }
 });
 
 //initial callback function for maps api
