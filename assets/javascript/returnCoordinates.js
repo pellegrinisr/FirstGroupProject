3009c5b48617e595b4221ea0923d24385d52a520
Stephen Pellegrini <spellegrini156@gmail.com>
2018-07-10 21:12:11 -0700

    on page load map shows overview of los angeles; users search queries change the content of the map

--- assets/javascript/returnCoordinates.js
+++ assets/javascript/returnCoordinates.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     var apiKey = "&key=AIzaSyC3sM_RkAYxBujzi6Qcox7GmkWQ1n-16Uc";
     var queryURL = "https://maps.googleapis.com/maps/api/geocode/json?address=";
-
+    var map;
     $('#btn-submit').on('click', function(event) {
         event.preventDefault();
         var searchTerm = $('#destination').val().replace(' ', '+');
@@ -14,6 +14,37 @@ $(document).ready(function() {
         }).then(function(response) {
             console.log(response);
             console.log(response.results[0].geometry.location.lat);
+            var latCord = response.results[0].geometry.location.lat;
+            console.log(response.results[0].geometry.location.lng);
+            var longCord = response.results[0].geometry.location.lng;
+            console.log($('#maps'));
+            map = new google.maps.Map(document.getElementById('map'), {
+                center: {lat: latCord, lng: longCord},
+                zoom: 15
+            });
         });
     });
 });
+    
+    
+
+    //function that will send returned coordinates to the google maps api
+//     function sendToMaps(lat, long) {
+        
+//         function initMap() {
+//             map = new google.maps.Map(document.getElementById('map'), {
+//                 center: {lat: lat, lng: long},
+//                 zoom: 8
+//             });
+//         }
+//     }
+// });
+
+
+//initial callback function for maps api
+function initialMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 34.040713, lng: -118.2467693},
+    zoom: 10
+    });
+}
