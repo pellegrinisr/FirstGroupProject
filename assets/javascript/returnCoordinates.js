999f924e024895dae973cff92b55d9ad02063ff3
Stephen Pellegrini <spellegrini156@gmail.com>
2018-07-11 18:21:18 -0700

    on click works for each marker

--- assets/javascript/returnCoordinates.js
+++ assets/javascript/returnCoordinates.js
@@ -1,8 +1,11 @@
 var map;
 
+
 $(document).ready(function() {
     var apiKey = "&key=AIzaSyC3sM_RkAYxBujzi6Qcox7GmkWQ1n-16Uc";
     var queryURL = "https://maps.googleapis.com/maps/api/geocode/json?address=";
+    var service;
+    var markerArray = [];
 
     $('#btn-submit').on('click', function(event) {
         event.preventDefault();
@@ -27,17 +30,30 @@ $(document).ready(function() {
                 
             });
 
+           var minPriceLevel;
+           var maxPriceLevel;
+           if ($('#radOne').prop('checked')) {
+                minPriceLevel = 1;
+                maxPriceLevel = 1;
+           } else if ($('#radTwo').prop('checked')) {
+               minPriceLevel = 2;
+               maxPriceLevel = 2;
+           } else {
+               minPriceLevel = 3;
+               maxPriceLevel = 4;
+           }
+
             var request = {
                 location: {lat: latCord, lng: longCord},
                 radius: '200',
                 query: 'restaurant',
-                minPriceLevel: 3
+                minPriceLevel: minPriceLevel,
+                maxPriceLevel: maxPriceLevel
             }
 
             service = new google.maps.places.PlacesService(map);
             service.textSearch(request, callback);
         });
-
     });
 
 
@@ -46,27 +62,43 @@ $(document).ready(function() {
             $('.output').html('');
             for (var i = 0; i < results.length; i++) {
                 var place = results[i];
-                if (place.rating >= 4) {
-                    var marker = new google.maps.Marker({
-                        position: {lat: place.geometry.location.lat(), lng: place.geometry.location.lng()},
-                        map: map
-                    })
-                    console.log(place);
-                    var newRowDiv = $('<div>').addClass('row');
-                    var newColDiv = $('<div>').addClass('col-12 output-item');
-                    // newColDiv.append('<p>').html(place.name);
-                    // newColDiv.append('<p>').html(place.formatted_address)
-                    var namePTag = $('<p>').html(place.name);
-                    var addressPTag = $('<p>').html(place.formatted_address);
-                    newColDiv.append(namePTag).append(addressPTag);
-                    newRowDiv.append(newColDiv);
-                    $('.output').append(newRowDiv);
-                }   
+                console.log('placeID: ' + place.place_id);
+
+                var marker = new google.maps.Marker({
+                    position: {lat: place.geometry.location.lat(), lng: place.geometry.location.lng()},
+                    map: map
+                });
+                markerArray.push(marker);
+                console.log(place);
+                var newRowDiv = $('<div>').addClass('row');
+                var newColDiv = $('<div>').addClass('col-12 output-item');
+                // newColDiv.append('<p>').html(place.name);
+                // newColDiv.append('<p>').html(place.formatted_address)
+                var namePTag = $('<p>').html(place.name);
+                var addressPTag = $('<p>').html(place.formatted_address);
+                var priceIcon = '';
+                if (place.price_level === 1) {
+                    priceIcon = '$';
+                } else if (place.price_level === 2) {
+                    priceIcon = '$$';
+                } else if (place.price_level === 3) {
+                    priceIcon = '$$$';
+                } else if (place.price_level === 4) {
+                    priceIcon = '$$$$';
+                } 
+                var costLevelPTag = $('<p>').html(priceIcon); 
+                var ratingPTag = $('<p>').html('Rating: ' + place.rating);
+                newColDiv.append(namePTag).append(addressPTag).append(ratingPTag).append(costLevelPTag);
+                newRowDiv.append(newColDiv);
+                $('.output').append(newRowDiv);   
+                google.maps.event.addListener(marker, 'click', function() {
+                    console.log(this.position.lat() + ', ' + this.position.lng());
+                    console.log(this);
+                    alert()
+                })
             }
         }
     }
-
-
 });
 
 //initial callback function for maps api
