2aea21bdd3ab32c8c08d5475a6182dcd4c4614a3
Stephen Pellegrini <spellegrini156@gmail.com>
2018-07-12 10:00:56 -0700

    wrote function to return geocoordinates from address string using places api call

--- assets/javascript/getCoordinates.js
+++ assets/javascript/getCoordinates.js
@@ -1,11 +1,17 @@
-var address = '21720 Thistledown Circle Yorba Linda Ca';
-var convertedAddress = address.replace(' ', '+');
-var apiKey = "&key=AIzaSyC3sM_RkAYxBujzi6Qcox7GmkWQ1n-16Uc";
-var queryURL = "https://maps.googleapis.com/maps/api/geocode/json?address=";
-
-$.ajax({
-    url: queryURL + convertedAddress + apiKey,
-    method: 'GET'
-}).then(function(response) {
-    console.log(response);
+$(document).ready(function() {
+    function getCoordinates(addressString) {
+        var convertedAddress = addressString.replace(' ', '+');
+        var apiKey = "&key=AIzaSyC3sM_RkAYxBujzi6Qcox7GmkWQ1n-16Uc";
+        var queryURL = "https://maps.googleapis.com/maps/api/geocode/json?address=";
+    
+        $.ajax({
+            url: queryURL + convertedAddress + apiKey,
+            method: 'GET'
+        }).then(function(response) {
+            console.log(response);
+        });
+    };
+    getCoordinates('21720 Thistledown Circle Yorba Linda Ca');
 });
+  
+   
