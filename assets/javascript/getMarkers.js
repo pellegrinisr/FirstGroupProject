a88e01e8d3a980a13b1d490ee9e08dfa04b547a6
Stephen Pellegrini <spellegrini156@gmail.com>
2018-07-13 10:40:32 -0700

    markers appear on page load

--- assets/javascript/getMarkers.js
+++ assets/javascript/getMarkers.js
@@ -1,41 +1,33 @@
 var map;
+var apiKey = "&key=AIzaSyC3sM_RkAYxBujzi6Qcox7GmkWQ1n-16Uc";
+var queryURL = "https://maps.googleapis.com/maps/api/geocode/json?address=";
+var service;
+$('#submit').on('click', function(event) {
+    // console.log(objectArray.length);
+    event.preventDefault();
 
-$(document).ready(function() {
- 
-    var apiKey = "&key=AIzaSyC3sM_RkAYxBujzi6Qcox7GmkWQ1n-16Uc";
-    var queryURL = "https://maps.googleapis.com/maps/api/geocode/json?address=";
-    var service;
-    $('#submit').on('click', function(event) {
-       // console.log(objectArray.length);
-        event.preventDefault();
-       for (var i = 0; i < objectArray.length; i++) {
-            console.log('before ajax call: ' + objectArray[i]);
-            //console.log(objectArray[i].name);
-            handleSearch(objectArray[i].location, objectArray[i].name)
-        }
-    })
-    
-    function handleSearch(searchTerm, placeName) {
-        //console.log($('#description').html());
-        $.ajax({
-            url: queryURL + searchTerm + apiKey,
-            method: "GET"
-        }).then(function(response) {
-            //console.log(response);
-           // console.log(objectArray[i].name)
-          console.log(placeName);
-           console.log('response: ' + JSON.stringify(response));
-           
-            var request = {
-                location: {lat: response.results[0].geometry.location.lat, lng: response.results[0].geometry.location.lng},
-                query: placeName
-            };
-            console.log(request);
-            service = new google.maps.places.PlacesService(map);
-            service.textSearch(request, placeQueryCallback);
-        });
-    }
-});
+})
+
+function handleSearch(searchTerm, placeName) {
+    //console.log($('#description').html());
+    $.ajax({
+        url: queryURL + searchTerm + apiKey,
+        method: "GET"
+    }).then(function(response) {
+        //console.log(response);
+        // console.log(objectArray[i].name)
+        console.log(placeName);
+        console.log('response: ' + JSON.stringify(response));
+        
+        var request = {
+            location: {lat: response.results[0].geometry.location.lat, lng: response.results[0].geometry.location.lng},
+            query: placeName
+        };
+        console.log(request);
+        service = new google.maps.places.PlacesService(map);
+        service.textSearch(request, placeQueryCallback);
+    });
+}
 
 
 //initial callback function for maps api
@@ -59,4 +51,7 @@ function initialMap() {
         center: {lat: 34.040713, lng: -118.2467693},
         zoom: 10
     }); 
+    for (var i = 0; i < objectArray.length; i++) {
+        handleSearch(objectArray[i].location, objectArray[i].name)
+    }
 }
