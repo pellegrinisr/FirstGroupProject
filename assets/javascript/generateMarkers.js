365f9f469ce010330ef3543f7ed4bf467d32e993
Stephen Pellegrini <spellegrini156@gmail.com>
2018-07-15 21:36:21 -0700

    scripts written to handle submit button click to grab the users choices from the selects; also will search the place api when the user clicks a marker

--- assets/javascript/generateMarkers.js
+++ assets/javascript/generateMarkers.js
@@ -1,16 +1,20 @@
+var markerArray = [];
+
 function placeMarker(coordinates, name) {
     var marker = new google.maps.Marker({
         position: coordinates,
         map: map,
         title: name
     });
+    markerArray.push(marker);
+    google.maps.event.addListener(marker, 'click', captureMarkerClicks);
 }
 
 
 function initialMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 34.040713, lng: -118.2467693},
-        zoom: 10
+        zoom: 11
     }); 
     for (var i = 0; i < objectArray.length; i++) {
         placeMarker(objectArray[i].coordinates, objectArray[i].name);
--- assets/javascript/objectArray3.js
+++ assets/javascript/objectArray3.js
@@ -1,127 +1,145 @@
 var objectArray = [
     {
         name: 'Felix',
-        location: 'Venice LA CA',
-        type: 'restaurant',
-        coordinates: {lat: 33.992677, lng: -118.471807}
+        location: 'West Los Angeles',
+        type: 'Restaurant',
+        coordinates: {lat: 33.992677, lng: -118.471807},
+        price: 3
     },
 
     {
         name: "Bestia",
-        location: "Downtown LA",
-        type: 'restaurant',
-        coordinates: {lat: 34.033762, lng: -118.229242}
+        location: "Downtown",
+        type: 'Restaurant',
+        coordinates: {lat: 34.033762, lng: -118.229242},
+        price: 3
     },
 
     {
         name: "Guelaguetza",
-        location: 'Koreatown LA',
-        type: 'restaurant',
-        coordinates: {lat: 34.052487, lng: -118.300722}
+        location: 'Koreatown',
+        type: 'Restaurant',
+        coordinates: {lat: 34.052487, lng: -118.300722}, 
+        price: 2
     },
 
     {
         name: 'Mariscos Jalisco',
-        location: 'Boyle Heights LA',
-        type: 'restaurant',
-        coordinates: {lat: 34.020945, lng: -118.212889}
+        location: 'East Los Angeles',
+        type: 'Restaurant',
+        coordinates: {lat: 34.020945, lng: -118.212889},
+        price: 1
     },
 
     {
         name: 'Guisados',
-        location: 'Downtown LA',
-        type: 'restaurant',
-        coordinates: {lat: 34.046708, lng: -118.250609}
+        location: 'Downtown',
+        type: 'Restaurant',
+        coordinates: {lat: 34.046708, lng: -118.250609},
+        price: 1
     },
 
     {
         name: 'Sun Nong Dang',
-        location: 'Koreatown LA',
-        type: 'restaurant',
-        coordinates: {lat: 34.063016, lng: -118.297504}
+        location: 'Koreatown',
+        type: 'Restaurant',
+        coordinates: {lat: 34.063016, lng: -118.297504},
+        price: 2
     },
 
     {
         name: 'Kobawoo',
-        location: 'Koreatown LA',
+        location: 'Koreatown',
         type: 'restaurat',
-        coordinates: {lat: 34.060134, lng: -118.290975}
+        coordinates: {lat: 34.060134, lng: -118.290975},
+        price: 2
     },
 
     {
         name: 'Gamja Gol',
-        location: 'Koreatown LA',
-        type: 'restaurant',
-        coordinates: {lat: 34.052887, lng: -118.300354}
+        location: 'Koreatown',
+        type: 'Restaurant',
+        coordinates: {lat: 34.052887, lng: -118.300354},
+        price: 2
     },
 
     {
         name: 'Beverly Soon Tofu',
-        location: 'Koreatown LA',
-        type: 'restaurant',
-        coordinates: {lat: 34.053122, lng: -118.292524}
+        location: 'Koreatown',
+        type: 'Restaurant',
+        coordinates: {lat: 34.053122, lng: -118.292524},
+        price: 2
     },
 
     {
         name: 'Plaza Mandarin House',
-        location: 'Koreatown Plaza LA',
-        type: 'restaurant',
-        coordinates: {lat: 34.055228, lng: -118.308444}
+        location: 'Koreatown',
+        type: 'Restaurant',
+        coordinates: {lat: 34.055228, lng: -118.308444},
+        price: 2
     },
 
     {
         name: 'Animal', 
-        location: 'Fairfax LA',
-        type: 'restaurant',
-        coordinates: {lat: 34.079316, lng: -118.361761}
+        location: 'Hollywood',
+        type: 'Restaurant',
+        coordinates: {lat: 34.079316, lng: -118.361761},
+        price: 3
     },
 
     {
         name: 'Tsujita LA',
         location: 'West Los Angeles',
-        type: 'restaurant',
-        coordinates: {lat: 34.039753, lng: -118.442687}
+        type: 'Restaurant',
+        coordinates: {lat: 34.039753, lng: -118.442687},
+        price: 2
     },
 
     {
         name: 'Kogi Taqueria',
-        locatioin: 'Palms LA',
-        type: 'restaurant',
-        coordinates: {lat: 34.024521, lng: -118.411203}
+        location: 'West Los Angeles',
+        type: 'Restaurant',
+        coordinates: {lat: 34.024521, lng: -118.411203},
+        price: 1
     },
 
     {
         name: 'Lasa',
-        location: 'Chinatown LA',
-        type: 'restaurant',
-        coordinates: {lat: 34.061491, lng: -118.239380}
+        location: 'Downtown',
+        type: 'Restaurant',
+        coordinates: {lat: 34.061491, lng: -118.239380},
+        price: 2
     }, 
 
     {
         name: 'Luv2eat Thai Bistro',
-        location: 'Hollywood LA',
-        type: 'restaurant',
-        coordinates: {lat: 34.097752, lng: -118.335917}
+        location: 'Hollywood',
+        type: 'Restaurant',
+        coordinates: {lat: 34.097752, lng: -118.335917},
+        price: 2
     },
 
     {
         name: 'Night + Market Song',
-        location: 'Silver Lake LA',
-        type: 'restaurant',
-        coordinates: {lat: 34.086916, lng: -118.275854}
+        location: 'Hollywood',
+        type: 'Restaurant',
+        coordinates: {lat: 34.086916, lng: -118.275854},
+        price: 2
     },
 
     {
         name: 'Kismet', 
-        location: 'Hollywood LA',
-        type: 'restaurant',
-        coordinates: {lat: 34.100235, lng: -118.290733}
+        location: 'Hollywood',
+        type: 'Restaurant',
+        coordinates: {lat: 34.100235, lng: -118.290733},
+        price: 2
     },
 
     {
         name: 'Churros don Abel',
-        location: 'East LA',
-        type: 'restaurant',
-        coordinates: {lat: 34.019081, lng: -118.152000}
+        location: 'East Los Angeles',
+        type: 'Restaurant',
+        coordinates: {lat: 34.019081, lng: -118.152000},
+        price: 1
     }    
 ]; 
